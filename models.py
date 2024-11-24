1b3f982b26a208c9223c2ec5b9808564f4077108
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-11-24 18:04:13 +0200

    Add ORM models

---
+++ models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+class Record(db.Model):
+    __tablename__ = 'records'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+
+    user = db.relationship('User', backref='records')
+    category = db.relationship('Category', backref='records')
