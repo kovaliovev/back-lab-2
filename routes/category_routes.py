333802ce203f093f49c5d38328b61afb3c5503dd
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-12-26 15:01:09 +0200

    Integrate category routes with db

--- routes/category_routes.py
+++ routes/category_routes.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify
-from data_store import categories, category_id_counter
-from marshmallow import ValidationError
+from models import Category, db
 from schemas import CategorySchema
+from marshmallow import ValidationError
 
 category_schema = CategorySchema()
 
@@ -9,26 +9,33 @@ def init_app(app):
     
     @app.route('/category', methods=['POST'])
     def create_category():
-        global category_id_counter
         try:
             data = request.get_json()
             validated_data = category_schema.load(data)
-            category = {'id': category_id_counter, 'name': validated_data['name']}
-            categories[category_id_counter] = category
-            category_id_counter += 1
-            return jsonify({'message': 'Category created', 'category_id': category['id']}), 201
+            category = Category(**validated_data)
+            db.session.add(category)
+            db.session.commit()
+            return jsonify({'message': 'Category created', 'category_id': category.id}), 201
         except ValidationError as err:
             return jsonify({'errors': err.messages}), 400
 
     @app.route('/category', methods=['GET'])
     def get_categories():
-        return jsonify(list(categories.values()))
+        categories = Category.query.all()
+        return jsonify(category_schema.dump(categories, many=True))
 
-    @app.route('/category', methods=['DELETE'])
-    def delete_category():
-        data = request.get_json()
-        category_id = data.get('category_id')
-        if category_id in categories:
-            del categories[category_id]
+    @app.route('/category/<int:category_id>', methods=['GET'])
+    def get_category(category_id):
+        category = Category.query.get(category_id)
+        if category:
+            return jsonify(category_schema.dump(category))
+        return jsonify({'message': 'Category not found'}), 404
+
+    @app.route('/category/<int:category_id>', methods=['DELETE'])
+    def delete_category(category_id):
+        category = Category.query.get(category_id)
+        if category:
+            db.session.delete(category)
+            db.session.commit()
             return jsonify({'message': 'Category deleted'})
         return jsonify({'message': 'Category not found'}), 404
