9ad6d721c1fcb7779da949f1426c6ca4c04913ed
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-12-26 13:12:08 +0200

    Add validation for record creating

--- routes/record_routes.py
+++ routes/record_routes.py
@@ -1,29 +1,37 @@
 from flask import request, jsonify
 from datetime import datetime
 from data_store import records, record_id_counter
+from marshmallow import ValidationError
+from schemas import RecordSchema
+
+record_schema = RecordSchema()
 
 def init_app(app):
     
     @app.route('/record', methods=['POST'])
     def create_record():
         global record_id_counter
-        data = request.get_json()
-        record = {
-            'id': record_id_counter,
-            'user_id': data['user_id'],
-            'category_id': data['category_id'],
-            'timestamp': datetime.now().isoformat(),
-            'amount': data['amount']
-        }
-        records[record_id_counter] = record
-        record_id_counter += 1
-        return jsonify({'message': 'Record created', 'record_id': record['id']}), 201
+        try:
+            data = request.get_json()
+            validated_data = record_schema.load(data)
+            record = {
+                'id': record_id_counter,
+                'user_id': validated_data['user_id'],
+                'category_id': validated_data['category_id'],
+                'timestamp': datetime.now().isoformat(),
+                'amount': validated_data['amount']
+            }
+            records[record_id_counter] = record
+            record_id_counter += 1
+            return jsonify({'message': 'Record created', 'record_id': record['id']}), 201
+        except ValidationError as err:
+            return jsonify({'errors': err.messages}), 400
 
     @app.route('/record/<int:record_id>', methods=['GET'])
     def get_record(record_id):
         record = records.get(record_id)
         if record:
-            return jsonify(record)
+          return jsonify(record)
         return jsonify({'message': 'Record not found'}), 404
 
     @app.route('/record/<int:record_id>', methods=['DELETE'])
