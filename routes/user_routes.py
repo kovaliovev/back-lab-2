03050c918f46d95858a2ec67b3407128ce0803f2
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-12-26 14:49:12 +0200

    Intergate user routes with db

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify
-from data_store import users, user_id_counter
-from marshmallow import ValidationError
+from models import User, db
 from schemas import UserSchema
+from marshmallow import ValidationError
 
 user_schema = UserSchema()
 
@@ -9,31 +9,33 @@ def init_app(app):
     
     @app.route('/user', methods=['POST'])
     def create_user():
-        global user_id_counter
         try:
             data = request.get_json()
             validated_data = user_schema.load(data)
-            user = {'id': user_id_counter, 'name': validated_data['name']}
-            users[user_id_counter] = user
-            user_id_counter += 1
-            return jsonify({'message': 'User created', 'user_id': user['id']}), 201
+            user = User(**validated_data)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({'message': 'User created', 'user_id': user.id}), 201
         except ValidationError as err:
             return jsonify({'errors': err.messages}), 400
 
     @app.route('/user/<int:user_id>', methods=['GET'])
     def get_user(user_id):
-        user = users.get(user_id)
+        user = User.query.get(user_id)
         if user:
-            return jsonify(user)
+            return jsonify(user_schema.dump(user))
         return jsonify({'message': 'User not found'}), 404
 
     @app.route('/user/<int:user_id>', methods=['DELETE'])
     def delete_user(user_id):
-        if user_id in users:
-            del users[user_id]
+        user = User.query.get(user_id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
             return jsonify({'message': 'User deleted'})
         return jsonify({'message': 'User not found'}), 404
 
     @app.route('/users', methods=['GET'])
     def get_users():
-        return jsonify(list(users.values()))
+        users = User.query.all()
+        return jsonify(user_schema.dump(users, many=True))
